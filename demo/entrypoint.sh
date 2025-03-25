52a88cf56fc768d93ac6c27c4e4efba8aa98b09e
Maryam Tahhan <mtahhan@redhat.com>
2025-03-25 16:39:43 +0000

    chore: reduce image size
    Signed-off-by: Maryam Tahhan <mtahhan@redhat.com>

--- demo/entrypoint.sh
+++ demo/entrypoint.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
 set -e
 
-# Add ROCm repo
-cat <<EOF > /etc/yum.repos.d/rocm.repo
-[ROCm]
-name=ROCm
-baseurl=https://repo.radeon.com/rocm/rhel9/6.2/main
-enabled=1
-gpgcheck=0
-EOF
-
-# Install runtime libraries
-microdnf install -y amd-smi-lib \
-        amd-smi \
-        miopen-hip \
-        rocm-core \
-        rocm-hip-libraries \
-        rocminfo \
-        llvm  \
-        clang \
-        lld
-
-# Clean up
-microdnf clean all
+python /workspace/triton-gpu-check.py
 
 # Execute the container’s default CMD or passed args
 exec "$@"
